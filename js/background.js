5fa52fbd7540ae9ebb0e000ff56a1cae38349bec
chengwei2 <chengwei2@JXWBJ1207261100.jiuxian.local>
2017-09-19 20:13:00 +0800

    update

--- js/background.js
+++ js/background.js
@@ -1,25 +1,28 @@
 function callResponse() {
-	$.get('https://www.baidu.com', function(html) {
-		var notificationId = util.openNewNotification({
-			type : 'image',
-			iconUrl : 'img/icon.png',
-			title : 'test',
-			message : 'Merry christmas!',
-			imageUrl : 'http://icon.nipic.com/BannerPic/20170815/original/20170815181219_1.jpg',
-			buttons : [ {
-				title : 'call',
+	$.get('http://127.0.0.1:9999/assistant-web?requestContent=getMsgFromLocalQueue', function(data) {
+		var msgList = $.parseJSON(data);
+		for(var index in msgList){
+			var notificationId = util.openNewNotification({
+				type : 'image',
 				iconUrl : 'img/icon.png',
-				click : function() {
-					alert('call telphone');
-				}
-			}, {
-				title : 'email',
-				iconUrl : 'img/icon.png',
-				click : function() {
-					alert('send email');
-				}
-			} ]
-		});
+				title : msgList[index].title,
+				message : msgList[index].content,
+				imageUrl : msgList[index].image,
+				buttons : [ {
+					title : 'call',
+					iconUrl : 'img/icon.png',
+					click : function() {
+						alert('call telphone');
+					}
+				}, {
+					title : 'email',
+					iconUrl : 'img/icon.png',
+					click : function() {
+						alert('send email');
+					}
+				} ]
+			});
+		}
 	});
 }
 util.openNewTab('http://127.0.0.1:9898/assistant-web');
