8cabc9d3fd5c7c1ec8160235228c7d562deb7ea5
chengwei2 <chengwei2@jiuxian.com>
2018-06-01 15:37:48 +0800

    update

--- js/texteditor.js
+++ js/texteditor.js
@@ -1,17 +1,71 @@
+var currentDate = (new Date()).format('yyyy-MM-dd');
+var fileNameKeyPrefix = "textEditorContent_";
+var historyFilesKey = "textEditorHistory";
+var textEditor;
+
+function init() {
+    var historyFiles = Settings.getObject(historyFilesKey);
+    console.log("historyFiles =>", historyFiles);
+    if (!historyFiles) {
+        historyFiles = [];
+        Settings.setObject(historyFilesKey, historyFiles);
+    }
+    if (historyFiles.indexOf(currentDate) == -1) {
+        historyFiles.push(currentDate);
+        Settings.setObject(historyFilesKey, historyFiles);
+    }
+    historyFiles.sort();
+    var template = $("<li class=\"h1_nav\"><a rel=\"nofollow\"></a></li>");
+    for (var index in historyFiles) {
+        var fileName = historyFiles[index];
+        var li = template.clone();
+        if (fileName == currentDate) {
+            li.addClass("selected");
+        }
+        li.attr("value", fileName);
+        var a = li.find("a");
+        a.attr("href", "#" + fileName);
+        a.html("记录[" + fileName + "]");
+        li.click(function () {
+            showTextByFileName(this);
+        });
+        $(".editor-container .editor-list ul").append(li);
+    }
+    resizeHeight();
+}
+
+function getFileNameKey() {
+    return fileNameKeyPrefix + $(".editor-container .editor-list ul li.selected").attr("value");
+}
+
+function showTextByFileName(that) {
+    $(".editor-container .editor-list ul li").removeClass("selected");
+    $(that).addClass("selected");
+    var content = Settings.getValue(getFileNameKey());
+    textEditor.setValue(content);
+}
+
+function resizeHeight() {
+    var realHeight = document.body.clientHeight - 65;
+    $(".editor-container .catalog-body").height(realHeight);
+}
+
 $(function () {
-    var testEditor;
-    var textContentKey = "textEditorContent";
-    var content = Settings.getValue(textContentKey);
+    init();
+    $(window).resize(function () {
+        resizeHeight();
+    });
+    var content = Settings.getValue(getFileNameKey());
     $("#test-editormd textarea").val(content);
-    testEditor = editormd("test-editormd", {
+    textEditor = editormd("test-editormd", {
         width: "100%",
         height: "100%",
         syncScrolling: "single",
-        path: "./js/" ,
-        onchange : function() {
+        path: "./js/",
+        onchange: function () {
             content = this.getValue();
             console.log("onchange =>", content);
-            Settings.setValue(textContentKey, content);
+            Settings.setValue(getFileNameKey(), content);
         }
     });
 });
